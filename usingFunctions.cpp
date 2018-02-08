617c4152b218fd9de7d2263acbc477ece913593b
BobbyRockers <rrockers@gmail.com>
2018-02-07 19:58:40 -0600

    Function examples

--- usingFunctions.cpp
+++ usingFunctions.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
 
+int large_number = 0;
+void printStuff(int);
+void printStuff(int,int);
+
 int main(){
-	cout << "Printing Stuff";
+	for(int i=0;i<10;i++){
+		large_number=large_number+i;
+		printStuff(i,large_number);
+		cout << "Printing with one argument" << endl;
+		printStuff(i);
+	}
+	
+	return 0;
+}
+
+void printStuff(int num){
+	cout << "Printing x5 " << (num * 5)  << " and total no total was given"  <<endl;
+}
+
+void printStuff(int num, int total){
+		cout << "Printing x5 " << (num * 5)  << " and total " << total <<endl;
 }
